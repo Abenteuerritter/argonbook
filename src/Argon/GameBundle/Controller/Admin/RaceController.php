8cb1e9961bd9f4a5f7eea547ebfc4dc672bcce51
Juan M <jm@guide42.com>
2014-01-19 18:16:56 +0100

    [Game] Admin Race

--- src/Argon/GameBundle/Controller/Admin/RaceController.php
+++ src/Argon/GameBundle/Controller/Admin/RaceController.php
@@ -3,14 +3,18 @@
 namespace Argon\GameBundle\Controller\Admin;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class RaceController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        $entities = $this->getDoctrine()
-                         ->getRepository('ArgonGameBundle:Race')
-                         ->findAll();
+        $repository = $this->getDoctrine()
+                           ->getRepository('ArgonGameBundle:Race');
+
+        $parentCode = $request->query->get('parent', null);
+        $parent     = $repository->findOneByCode($parentCode);
+        $entities   = $repository->findByParent($parent);
 
         return $this->render('ArgonGameBundle:Admin\Race:index.html.twig', array(
             'entities' => $entities,
