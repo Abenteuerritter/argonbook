44234e775a20a8714b83eee9ca52362c7608f4eb
Juan M <jm@guide42.com>
2014-02-01 13:16:41 +0100

    [Style] Breadcrumb

--- src/Argon/GameBundle/Controller/Admin/CharacterController.php
+++ src/Argon/GameBundle/Controller/Admin/CharacterController.php
@@ -11,15 +11,19 @@ class CharacterController extends Controller
 {
     public function indexAction(Request $request)
     {
-        $game = $request->query->get('game');
-
         $repository = $this->getDoctrine()
                            ->getRepository('ArgonGameBundle:Character');
 
-        if (null === $game) {
-            $entities = $repository->findAll();
+        $game = null;
+
+        if ($request->query->has('game')) {
+            $gameName    = $request->query->get('game');
+            $gameFactory = $this->container->get('argon_game.provider.game_factory');
+            $game        = $gameFactory->create($gameName);
+
+            $entities = $repository->findByGameName($gameName);
         } else {
-            $entities = $repository->findByGameName($game);
+            $entities = $repository->findAll();
         }
 
         return $this->render('ArgonGameBundle:Admin\Character:index.html.twig', array(
