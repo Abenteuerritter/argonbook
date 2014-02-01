ff0f49133baeb7e426090f5f4e46a003468a43e3
Juan M <jm@guide42.com>
2014-02-01 03:46:58 +0100

    [Game] Add more info to Character listing in BO

--- src/Argon/GameBundle/Controller/Admin/CharacterController.php
+++ src/Argon/GameBundle/Controller/Admin/CharacterController.php
@@ -3,16 +3,25 @@
 namespace Argon\GameBundle\Controller\Admin;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class CharacterController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        $entities = $this->getDoctrine()
-                         ->getRepository('ArgonGameBundle:Character')
-                         ->findAll();
+        $game = $request->query->get('game');
+
+        $repository = $this->getDoctrine()
+                           ->getRepository('ArgonGameBundle:Character');
+
+        if (null === $game) {
+            $entities = $repository->findAll();
+        } else {
+            $entities = $repository->findByGameName($game);
+        }
 
         return $this->render('ArgonGameBundle:Admin\Character:index.html.twig', array(
+            'game'     => $game,
             'entities' => $entities,
         ));
     }
