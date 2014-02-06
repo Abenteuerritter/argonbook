a229b9d51e359673243a1bc0ce638354ef0386a3
Juan M <jm@guide42.com>
2014-02-06 09:32:32 +0100

    [Game][Skill] Add fixtures

--- src/Argon/GameBundle/DataFixtures/ORM/SkillFixture.php
+++ src/Argon/GameBundle/DataFixtures/ORM/SkillFixture.php
@@ -11,6 +11,7 @@ class SkillFixture implements FixtureInterface
 {
     public function load(ObjectManager $manager)
     {
+        // Strength / Stärke
         $strWeaponUse = $this->createSkill($manager, 'STR', 'Weapon Use', 'Waffengebrauch');
         $strTwoWeapons = $this->createSkill($manager, 'STR', 'Two Weapons', 'Zwei Waffen', $strWeaponUse);
 
@@ -27,6 +28,50 @@ class SkillFixture implements FixtureInterface
         $this->createSkill($manager, 'STR', 'Battle Cry', 'Kampruf', $strTwoWeapons, 2);
         $this->createSkill($manager, 'STR', 'Fortitude', 'Die Kraft', null, 2);
         $this->createSkill($manager, 'STR', 'Stamina (Extra Life Points)', 'Ausdauer (Zusätzlicher Lebenspunkt)', null, 2, 8);
+
+        // Dexterity / Geschicklichkeit
+        $this->createSkill($manager, 'DEX', 'Throwing Weapon', 'Wurfgeschoss');
+        $this->createSkill($manager, 'DEX', 'Bow - Crossbow', 'Bogen - Armbrust');
+        $this->createSkill($manager, 'DEX', 'Stealth', 'Verstecken im Schatten');
+
+        $dexHideSearch = $this->createSkill($manager, 'DEX', 'Hide-Search', 'Entstecken-Suchen');
+
+        $this->createSkill($manager, 'DEX', 'Escape Artist', 'Ausbruchsmeister');
+        $this->createSkill($manager, 'DEX', 'Find/Remove/Build traps', 'Fallen finden/entschärfen/bauen');
+        $this->createSkill($manager, 'DEX', 'Open/Build locks', 'Schlösser öffnen\bauen');
+
+        $dexThievery = $this->createSkill($manager, 'DEX', 'Thievery', 'Diebstahl');
+
+        $this->createSkill($manager, 'DEX', 'Pickpocketing', 'Taschendiebstahl', $dexThievery, 2);
+        $this->createSkill($manager, 'DEX', 'Shoplifting', 'Warendiebstahl', $dexThievery, 2);
+        $this->createSkill($manager, 'DEX', 'Burglary', 'Einbruch', $dexThievery, 2);
+
+        $dexStrangle = $this->createSkill($manager, 'DEX', 'Strangle', 'Erwürgen', null, 2);
+        $dexKnockUnconscious = $this->createSkill($manager, 'DEX', 'Knock unconscious', 'Niederschlagen', $dexStrangle, 2);
+
+        $this->createSkill($manager, 'DEX', 'Assasinate', 'Meucheln', $dexKnockUnconscious, 2);
+        $this->createSkill($manager, 'DEX', 'The Reflex', 'Der Reflex', null, 2);
+
+        // Wisdom / Weisheit
+        $this->createSkill($manager, 'WIS', 'Reading & Writing', 'Lesen und Schreiben');
+
+        $wisFirstAid = $this->createSkill($manager, 'WIS', 'First Aid', 'Erste Hilfe');
+
+        $this->createSkill($manager, 'WIS', 'Healing arts', 'Heilkunde', $wisFirstAid, 1, 9);
+        $this->createSkill($manager, 'WIS', 'Tales and Legends', 'Geschichten und Legenden', null, 1, 2);
+        $this->createSkill($manager, 'WIS', 'Extra Magic Points', 'Zusätzliche Magiepunkte');
+
+        $wisMagicKnowledge = $this->createSkill($manager, 'WIS', 'Magic Knowledge', 'Magie durchschauen');
+
+        $this->createSkill($manager, 'WIS', 'Magic', 'Magie', $wisMagicKnowledge);
+
+        $wisHerbalist = $this->createSkill($manager, 'WIS', 'Herbalist', 'Kreuterkenner');
+
+        $this->createSkill($manager, 'WIS', 'Alchemy', 'Alchemie', $wisHerbalist, 2);
+        $this->createSkill($manager, 'WIS', 'Identify Magic Object', 'Magische Dinge erkennen', null, 2);
+        $this->createSkill($manager, 'WIS', 'Create Magic Object', 'Magische Objekt Erzeugen', null, 2); // TODO requieres adecuates Handcraft
+        $this->createSkill($manager, 'WIS', 'Tracking', 'Verfolgen', $dexHideSearch, 2);
+        $this->createSkill($manager, 'WIS', 'The Will', 'Der Wille', null, 2);
 
         $manager->flush();
     }
