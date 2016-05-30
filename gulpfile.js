e3dc001eca10681c50d115622cb4c27c7b078709
Juan M <jm@guide42.com>
2016-05-30 00:23:45 -0300

    Compile to Bundle

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,9 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
  
-var SASS_ENTRYPOINT = "src/Argon/WebBundle/Resources/scss/argonbook.scss";
-var SASS_OPTIONS    = {outputStyle: "compressed", includePaths: ["node_modules"]};
+var SASS_ENTRYPOINT  = "src/Argon/WebBundle/Resources/scss/argonbook.scss";
+var SASS_DESTINATION = "src/Argon/WebBundle/Resources/public/css/";
+var SASS_OPTIONS     = {outputStyle: "compressed", includePaths: ["node_modules"]};
 
 gulp.task('default', ['sass']);
 
@@ -12,7 +13,7 @@ gulp.task('sass', function() {
   console.info("+ " + SASS_ENTRYPOINT.toString());
   return gulp.src(SASS_ENTRYPOINT)
     .pipe(sass.sync(SASS_OPTIONS).on('error', sass.logError))
-    .pipe(gulp.dest('web/css/'));
+    .pipe(gulp.dest(SASS_DESTINATION));
 });
 
 gulp.task('sass:watch', function() {
