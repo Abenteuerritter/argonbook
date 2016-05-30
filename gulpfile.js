445d2c9d21c663a0ed8be6827f661536617caeb4
Juan M <jm@guide42.com>
2016-05-29 21:48:21 -0300

    Back to npm/gulp

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+"use strict";
+ 
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+ 
+var SASS_ENTRYPOINT = "src/ArgonWeb/sass/argonbook.scss";
+
+gulp.task('default', ['sass']);
+
+gulp.task('sass', function() {
+  console.info("+ " + SASS_ENTRYPOINT.toString());
+  return gulp.src(SASS_ENTRYPOINT)
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest('web/css/'));
+});
+
+gulp.task('sass:watch', function() {
+  gulp.watch(SASS_ENTRYPOINT, ['sass']);
+});
