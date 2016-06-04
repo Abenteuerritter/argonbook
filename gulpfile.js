03457cd067e17920d0a7dc97cb319ab70f81afab
Juan M <jm@guide42.com>
2016-06-04 14:17:18 -0300

    Style

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,40 @@
-"use strict";
+'use strict';
  
-var gulp = require("gulp");
-var sass = require("gulp-sass");
- 
-var SASS_ENTRYPOINT  = "src/Argon/WebBundle/Resources/scss/argonbook.scss";
-var SASS_DESTINATION = "src/Argon/WebBundle/Resources/public/css/";
-var SASS_OPTIONS     = {outputStyle: "compressed", includePaths: ["node_modules"]};
+var gulp   = require('gulp');
+var util   = require('gulp-util');
+var sass   = require('gulp-sass');
+var concat = require('gulp-concat');
 
-gulp.task('default', ['sass']);
+var // Configuration
+  SASS_DEST    = 'web/css/',
+  SASS_SRC     = 'app/assets/scss/argonbook.scss',
+  SASS_OPTIONS = {outputStyle: 'compressed', includePaths: ['node_modules']},
+
+  JS_DEST = 'web/js/',
+  JS_SRC  = [
+    'node_modules/jquery/dist/jquery.min.js',
+    'node_modules/foundation-sites/dist/foundation.min.js',
+  ]
+;
+
+gulp.task('default', ['sass', 'js']);
 
 gulp.task('sass', function() {
-  console.info("+ " + SASS_ENTRYPOINT.toString());
-  return gulp.src(SASS_ENTRYPOINT)
+  return gulp.src(SASS_SRC)
     .pipe(sass.sync(SASS_OPTIONS).on('error', sass.logError))
-    .pipe(gulp.dest(SASS_DESTINATION));
+    .pipe(gulp.dest(SASS_DEST));
 });
 
 gulp.task('sass:watch', function() {
-  gulp.watch(SASS_ENTRYPOINT, ['sass']);
+  gulp.watch(SASS_SRC, ['sass']);
+});
+
+gulp.task('js', function() {
+  return gulp.src(JS_SRC)
+    .pipe(concat('vendor.js'))
+    .pipe(gulp.dest(JS_DEST));
+});
+
+gulp.task('js:watch', function() {
+  gulp.watch(JS_SRC, ['js']);
 });
