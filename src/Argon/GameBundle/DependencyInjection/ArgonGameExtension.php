11d6ba1470b9005eb0e049294763246ba8ca8fb4
Juan M <jm@guide42.com>
2016-08-27 16:41:21 -0300

    Revert "Remove game form services definition"
    This reverts commit 9b6f53c505baea8c8134665a1bd5f30340b58d88.

--- src/Argon/GameBundle/DependencyInjection/ArgonGameExtension.php
+++ src/Argon/GameBundle/DependencyInjection/ArgonGameExtension.php
@@ -12,6 +12,7 @@ class ArgonGameExtension extends Extension
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('services/forms.xml');
         $loader->load('services/games.xml');
         $loader->load('services/listeners.xml');
         $loader->load('services/security.xml');
