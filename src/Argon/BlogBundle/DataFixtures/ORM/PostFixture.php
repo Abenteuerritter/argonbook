58e8fe8598e771db21214856a8720677a41415d4
Juan M <jm@guide42.com>
2016-09-02 20:43:56 -0300

    Simple blog post

--- src/Argon/BlogBundle/DataFixtures/ORM/PostFixture.php
+++ src/Argon/BlogBundle/DataFixtures/ORM/PostFixture.php
@@ -17,13 +17,7 @@ class PostFixture implements FixtureInterface
         $hello->setCreator($root);
         $hello->setTitle('Hello World');
         $hello->setBody(<<<BODY
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a lacus justo.
-Quisque a risus rhoncus, facilisis urna vel, varius purus. Sed pellentesque ex
-volutpat, maximus ligula sed, vulputate lectus. Nunc dictum aliquam magna non
-iaculis. Curabitur posuere maximus tempus. Praesent rhoncus diam orci, sit
-amet bibendum lorem volutpat et. Suspendisse aliquet risus et est accumsan, a
-pharetra sapien volutpat. Proin iaculis varius justo, fermentum lacinia metus
-elementum et.
+Hello. This is Argonbook's first blog post.
 BODY
         );
 
