520220cc0c279690117e962d9c9e5a1d56960a4c
Juan M <jm@guide42.com>
2017-01-08 23:35:34 +0100

    Edit race, show genres

--- src/Argon/GameBundle/Controller/Admin/RaceController.php
+++ src/Argon/GameBundle/Controller/Admin/RaceController.php
@@ -3,8 +3,11 @@
 namespace Argon\GameBundle\Controller\Admin;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 use Argon\GameBundle\Entity\Race;
+use Argon\GameBundle\Form\Race\RaceEditType;
 
 class RaceController extends Controller
 {
@@ -21,6 +24,36 @@ class RaceController extends Controller
     {
         return $this->render('ArgonGameBundle:Admin/Race:view.html.twig', array(
             'entity' => $race,
+        ));
+    }
+
+    public function editAction(Race $race, Request $request)
+    {
+        $form = $this->createForm(RaceEditType::class, $race, array(
+            'action' => $this->generateUrl('admin_race_edit_update', array('slug' => $race->getSlug())),
+            'method' => 'POST',
+        ));
+
+        $form->add('submit', SubmitType::class, array(
+            'label' => 'admin.race.edit_submit',
+            'attr'  => array('class' => 'button'),
+        ));
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager = $this->getDoctrine()->getManager();
+            $manager->flush();
+
+            $request->getSession()->getFlashBag()
+                    ->add('success', 'admin.race.updated');
+
+            return $this->redirect($this->generateUrl('admin_race_view', array('slug' => $race->getSlug())));
+        }
+
+        return $this->render('ArgonGameBundle:Admin/Race:edit.html.twig', array(
+            'race' => $race,
+            'form' => $form->createView(),
         ));
     }
 
