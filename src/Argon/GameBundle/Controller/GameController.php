fabfe5288ab248afedd48c834089cfbfbe765a05
Juan M <jm@guide42.com>
2017-01-10 00:31:10 +0100

    Game listing

---
+++ src/Argon/GameBundle/Controller/GameController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Argon\GameBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use Argon\GameBundle\Provider\GameInterface;
+
+class GameController extends Controller
+{
+    public function indexAction()
+    {
+        $gameFactory = $this->get('argon_game.provider.game_factory');
+        $games       = $gameFactory->getGames();
+
+        return $this->render('ArgonGameBundle:Game:index.html.twig', array(
+            'games' => $games,
+        ));
+    }
+
+    public function viewAction(GameInterface $game)
+    {
+    }
+}
