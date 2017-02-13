1ff985b5f3177d92d08adae61e2eb7e7e3f1c4d8
Juan M <jm@guide42.com>
2017-02-13 19:04:57 +0100

    Insert races with genres in fixtures

--- src/Argon/GameBundle/DataFixtures/ORM/RaceFixture.php
+++ src/Argon/GameBundle/DataFixtures/ORM/RaceFixture.php
@@ -11,23 +11,24 @@ class RaceFixture implements FixtureInterface
 {
     public function load(ObjectManager $manager)
     {
-        $this->createRace($manager, 'Elf', 'Elf');
-        $this->createRace($manager, 'High Elf', 'Hochelf');
-        $this->createRace($manager, 'Dark Elf', 'Dunkelelf (Drow)');
-        $this->createRace($manager, 'Wild Elf', 'Wild-Elf');
-        $this->createRace($manager, 'Human', 'Mensch');
-        $this->createRace($manager, 'Orc', 'Orc');
-        $this->createRace($manager, 'Half Orc', 'Halb Orc');
-        $this->createRace($manager, 'Half Elf', 'Halb Elf');
-        $this->createRace($manager, 'Goblin', 'Kobold');
-        $this->createRace($manager, 'Dwarf', 'Zwerg');
-        $this->createRace($manager, 'Hobbit', 'Hobbit');
-        $this->createRace($manager, 'Fairy', 'Fee');
-        $this->createRace($manager, 'Gnome', 'Gnom');
+        $this->createRace($manager, 'Elf',      'Elf',              ['MID']);
+        $this->createRace($manager, 'High Elf', 'Hochelf',          ['MID']);
+        $this->createRace($manager, 'Dark Elf', 'Dunkelelf (Drow)', ['MID']);
+        $this->createRace($manager, 'Wild Elf', 'Wild-Elf',         ['MID']);
+        $this->createRace($manager, 'Human',    'Mensch',           ['MID']);
+        $this->createRace($manager, 'Orc',      'Orc',              ['MID']);
+        $this->createRace($manager, 'Half Orc', 'Halb Orc',         ['MID']);
+        $this->createRace($manager, 'Half Elf', 'Halb Elf',         ['MID']);
+        $this->createRace($manager, 'Goblin',   'Kobold',           ['MID']);
+        $this->createRace($manager, 'Dwarf',    'Zwerg',            ['MID']);
+        $this->createRace($manager, 'Hobbit',   'Hobbit',           ['MID']);
+        $this->createRace($manager, 'Fairy',    'Fee',              ['MID']);
+        $this->createRace($manager, 'Gnome',    'Gnom',             ['MID']);
+
         $manager->flush();
     }
 
-    protected function createRace(ObjectManager $manager, $name, $nameDE = null, $modifier = 1)
+    protected function createRace(ObjectManager $manager, $name, $nameDE = null, array $genres = [], $modifier = 1)
     {
         /** @var \Gedmo\Translatable\Entity\Repository $repository */
         $repository = $manager->getRepository('Gedmo\\Translatable\\Entity\\Translation');
@@ -35,6 +36,7 @@ class RaceFixture implements FixtureInterface
         $race = new Race();
         $race->setName($name);
         $race->setModifier($modifier);
+        $race->setGenres($genres);
 
         if ($nameDE !== null) {
             $repository->translate($race, 'name', 'de', $nameDE);
