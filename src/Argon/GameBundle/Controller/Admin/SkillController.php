d08fa52a766fe7fb201324482b43b4acb94d8e76
Juan M <jm@guide42.com>
2017-04-02 17:59:52 +0200

    Admin skill edit

--- src/Argon/GameBundle/Controller/Admin/SkillController.php
+++ src/Argon/GameBundle/Controller/Admin/SkillController.php
@@ -4,8 +4,10 @@ namespace Argon\GameBundle\Controller\Admin;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 use Argon\GameBundle\Entity\Skill;
+use Argon\GameBundle\Form\Skill\SkillEditType;
 
 class SkillController extends Controller
 {
@@ -32,6 +34,39 @@ class SkillController extends Controller
 
         return $this->render('ArgonGameBundle:Admin/Skill:view.html.twig', array(
             'entity' => $skill,
+        ));
+    }
+
+    public function editAction(Request $request, $slug)
+    {
+        /** @var Skill $skill */
+        $skill = $this->getRepository()->findOneBySlug($slug);
+
+        $form = $this->createForm(SkillEditType::class, $skill, array(
+            'action' => $this->generateUrl('admin_skill_edit_update', array('slug' => $skill->getSlug())),
+            'method' => 'POST',
+        ));
+
+        $form->add('submit', SubmitType::class, array(
+            'label' => 'admin.skill.edit_submit',
+            'attr'  => array('class' => 'button'),
+        ));
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager = $this->getDoctrine()->getManager();
+            $manager->flush();
+
+            $request->getSession()->getFlashBag()
+                    ->add('success', 'admin.skill.updated');
+
+            return $this->redirect($this->generateUrl('admin_skill_view', array('slug' => $skill->getSlug())));
+        }
+
+        return $this->render('ArgonGameBundle:Admin/Skill:edit.html.twig', array(
+            'skill' => $skill,
+            'form'  => $form->createView(),
         ));
     }
 
