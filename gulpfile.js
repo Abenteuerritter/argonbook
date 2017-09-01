b0bbd64fc00fce4449f50003fb0c5c11162fdc59
Juan M <jm@guide42.com>
2017-09-02 01:16:09 +0200

    CS

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ var plumber = require('gulp-plumber');
 var // Configuration
   SASS_DEST    = 'web/css/',
   SASS_SRC     = 'app/assets/scss/argonbook.scss',
-  SASS_OPTIONS = {outputStyle: 'compressed', includePaths: ['node_modules', 'bower_components']},
+  SASS_OPTIONS = { outputStyle: 'compressed', includePaths: ['node_modules', 'bower_components'] },
 
   JS_DEST = 'web/js/',
   JS_SRC  = [
